ae7d4926458b779ea87903978adc8455c3e71cfc
satendra3406 <satendragond@startbitsolutions.com>
2021-04-08 18:15:42 +0530

    Payment Failed Issues Solved using credential edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "custom/credit_card_form"
 
 Rails.start()
 Turbolinks.start()
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,20 +1,20 @@
-class Payment < ActiveRecord::Base
-	belongs_to :user
-	attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+class Payment < ApplicationRecord
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+  belongs_to :user
+  def self.month_options
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1] }
+  end
+  def self.year_options
+    (Date.today.year..(Date.today.year+10)).to_a
+  end
 
-	def self.month_options
-		Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
-	end
+  def process_payment
+  	charge = Stripe::Charge.create({
+      amount: 1000,
+      currency: 'inr',
+      description: 'Premium',
+      source: token
+    })
+     end
 
-	def self.year_options
-		(Date.today.year..(Date.today.year+10)).to_a
-	end
-
-	def process_payment
-		customer = Stripe::Customer.create email: email, card: token
-		Stripe::Charge.create customer: customer.id,
-		amount: 1000,
-		description: 'Premium',
-		currency: 'usd'
-	end
 end
