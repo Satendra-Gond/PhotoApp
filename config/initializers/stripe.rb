7281cd24bf93cc034719e840c3ebc70a6c3b33b2
satendra3406 <satendragond@startbitsolutions.com>
2021-04-08 18:28:24 +0530

    Payment Failed Issues Solved using credential edit

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-	publishable_key: ENV['PUBLISHABLE_KEY'] ||= Rails.application.credentials.stripe[:PUBLISHABLE_KEY],
-	secret_key: ENV['SECRET_KEY'] ||= Rails.application.credentials.stripe[:SECRET_KEY]
+	publishable_key: (ENV['PUBLISHABLE_KEY'] ||= Rails.application.credentials.stripe[:PUBLISHABLE_KEY]),
+	secret_key: (ENV['SECRET_KEY'] ||= Rails.application.credentials.stripe[:SECRET_KEY])
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
